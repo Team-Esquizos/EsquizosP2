9c456c55462c5657f250be2d8d04590ce751cfaa
Jamkkles <pcorrea21@alumnos.utalca.cl>
2024-11-09 18:27:28 -0300

    SCRUM-6 carpeta de router y avance en la interfaz de administradores

--- frontend-xquizos/src/App.vue
+++ frontend-xquizos/src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <Login/>
+<router-view/>
 </template>
 
 <script>
 
-import Login from './components/Login.vue';
 
 
 export default {
   name: 'App',
-  components: {
-    Login
-  }
+
 }
 </script>
 
