a797d541e71d92ffd77582ed61d7307c7e44aa13
estudiantedesesperado <aufuenzalida20@alumnos.utalca.cl>
2024-11-10 13:51:46 -0300

    SCRUM-3 arreglo en las dependencias

--- frontend-xquizos/src/main.js
+++ frontend-xquizos/src/main.js
@@ -2,6 +2,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // Importa el enrutador
+import 'flowbite/dist/flowbite.css';
 
 const app = createApp(App);
 
