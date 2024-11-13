50e2eaa9c25bce02bd533a9efa69283f6cc22ab9
Xpell <321seba1retamal2r@gmail.com>
2024-11-13 18:54:44 -0300

    SCRUM-15 envio de estudiante al back por csv

--- Backend-xquizos/src/routes/enviocsvroute.js
+++ Backend-xquizos/src/routes/enviocsvroute.js
@@ -24,12 +24,15 @@ var storage = multer.diskStorage({
 
 var upload = multer({storage:storage});
 
-const datosController = require('../controllers/datosController');
+
 const cursoController = require('../controllers/cursoController');
 const profesorController = require('../controllers/ProfesorController');
+const estudianteController = require('../controllers/EstudianteController');
 
-datos.post('/importDatos',upload.single('file'),datosController.importDatos);
+
 datos.post('/importCurso',upload.single('file'),cursoController.importCurso);
 datos.post('/importProfesor',upload.single('file'),profesorController.importProfesor);
+datos.post('/importEstudiante',upload.single('file'),estudianteController.importEstudiante);
+
 datos.get('/getCurso',cursoController.getCurso);
 module.exports = datos;
