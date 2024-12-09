8a9440c1183bb585dd51a25dd3ffedbc38f4d44d
Xhiah <ifarias21@alumnos.utalca.cl>
2024-12-08 23:56:32 -0300

    SCRUM-46 Corrección pequeño error 2
    Pequeño cambios

--- frontend-xquizos/src/components/Profile.vue
+++ frontend-xquizos/src/components/Profile.vue
@@ -21,11 +21,10 @@
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-              class="si"
                 d="M19 4v6.406l-3.753 3.741-6.463-6.462 3.7-3.685h6.516zm2-2h-12.388l1.497 1.5-4.171 4.167 9.291 9.291 4.161-4.193 1.61 1.623v-12.388zm-5 4c.552 0 1 .449 1 1s-.448 1-1 1-1-.449-1-1 .448-1 1-1zm0-1c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2zm6.708.292l-.708.708v3.097l2-2.065-1.292-1.74zm-12.675 9.294l-1.414 1.414h-2.619v2h-2v2h-2v-2.17l5.636-5.626-1.417-1.407-6.219 6.203v5h6v-2h2v-2h2l1.729-1.729-1.696-1.685z"
               ></path>
             </svg>
-            <span class="si">Editar Imagen</span>
+            <span>Editar Imagen</span>
           </button>
           <input type="file" ref="fileInput" @change="onFileSelected" style="display: none;" accept="image/*" />
         </li>
@@ -42,11 +41,10 @@
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                class="si"
                 d="M2.598 9h-1.055c1.482-4.638 5.83-8 10.957-8 6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5c-5.127 0-9.475-3.362-10.957-8h1.055c1.443 4.076 5.334 7 9.902 7 5.795 0 10.5-4.705 10.5-10.5s-4.705-10.5-10.5-10.5c-4.568 0-8.459 2.923-9.902 7zm12.228 3l-4.604-3.747.666-.753 6.112 5-6.101 5-.679-.737 4.608-3.763h-14.828v-1h14.826z"
               ></path>
             </svg>
-            <span><a href="#" @click="cerrarSesion" class="si">Salir</a></span>
+            <span><a href="#" @click="cerrarSesion">Salir</a></span>
           </button>
         </li>
       </ul>
@@ -329,6 +327,7 @@ export default {
 .popup-window ul button:hover svg,
 .popup-window ul button:focus-visible svg {
   color: var(--nav-button-hover-text-color);
+  background: var(--nav-button-hover-bg);
 }
 
 /* Efectos de escala en el botón de perfil */
@@ -352,7 +351,9 @@ export default {
   opacity: 1;
 }
 
-.si{
-  color: #000000;
+a {
+  color: inherit; /* Conserva el color predeterminado del texto */
+  text-decoration: none; /* Quita el subrayado predeterminado */
 }
+
 </style>
